201fdd4dd87a044f311b8db2eef5a034a118bdf4
Marcela Trujillo Toro <mt@fullstacksolvers.com>
2024-06-07 11:59:23 -0500

    feat: routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404PageComponent } from './shared/pages/error-404-page/error-404-page.component';
 
-const routes: Routes = [];
+// localhost:/
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./auth/auth.module').then((module) => module.AuthModule),
+  },
+  {
+    path: 'heroes',
+    loadChildren: () =>
+      import('./heroes/heroes.module').then((module) => module.HeroesModule),
+  },
+  {
+    path: '404',
+    component: Error404PageComponent,
+  },
+  { path: '', redirectTo: 'heroes', pathMatch: 'full' },
+  { path: '**', redirectTo: '404' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
